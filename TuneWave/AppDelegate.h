da589f7cafdd5dd573bd2b8b6b30c09c5fe948e8
Pankaj Kumar Jha <pankaj@aleph-labs.com>
2018-01-10 23:28:31 +0800

    Initial Commit

---
+++ TuneWave/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TuneWave
+//
+//  Created by Pankaj on 10/01/18.
+//  Copyright © 2018 Aleph-Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
